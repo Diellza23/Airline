2d5d5aefd29bdbe7ea68c377f04b9d3806bf20b9
Diellza23 <dk49556@ubt-uni.net>
2021-09-07 02:00:22 +0200

    Changes

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -16,18 +16,18 @@ export default observer (function LoginForm() {
         >
             {({handleSubmit, isSubmitting, errors}) =>(
                 <Form className='ui form' onSubmit={handleSubmit} autoComplete='off' style={{height:"400px",padding:"15px"}}>
-                    <h2 style={{textAlign:"center",color:"#62676e",marginBottom:"30px",fontFamily:"arial"}}>Login</h2>
+                    <h2 style={{textAlign:"center",color:"rgba(224, 130, 131, 1)",marginBottom:"30px",fontFamily:"arial"}}>Login</h2>
                     <h4>Email:</h4>
-                    <MyTextInput name='email' placeholder='Example@net.com'/>
+                    <MyTextInput name='email' placeholder='example@net.com'/>
                     <h4>Password:</h4>
-                    <MyTextInput name='password' placeholder='Password' type='password'/>
+                    <MyTextInput name='password' placeholder='password' type='password'/>
                     
                     <ErrorMessage
                         name='error' render={() => <Label style={{marginBottom:10}} basic color='red' content={errors.error}/>}
                     />
                     <Button loading={isSubmitting} positive content='Login' type='submit' fluid 
                     style={{borderRadius:"20px",backgroundColor:"rgba(241, 130, 141,1)",color:"white"}}/>
-                    <h5 style={{textAlign:"center"}}>Follow us</h5>
+                    <h5 style={{textAlign:"center"}}>Follow us.</h5>
                     <div style={{display:"flex", justifyContent:"center"}}>
                     <a href="https://www.facebook.com" target="_blank" style={{paddingRight:"15px"}}>
                     <img src="/assets/fblogo.png"  width="22" height="22"  /></a>
